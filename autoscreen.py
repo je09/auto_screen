e671aed71ac98073dfabb389c313a12a9cbc6a8c
je09 <je09@Yandex.ru>
2022-02-21 11:55:36 +0500

    Timeout increased

--- autoscreen.py
+++ autoscreen.py
@@ -1,13 +1,15 @@
 #!/bin/python
-from PIL import ImageChops # $ pip install pillow
-import pyscreenshot as ImageGrab # $ pip install pyscreenshot
+from PIL import ImageChops
+import pyscreenshot as ImageGrab
 from time import sleep
 from datetime import datetime
 
-image = ImageGrab.grab().convert('RGB')
+TIMEOUT = 30 # Seconds
+
+image = ImageGrab.grab().convert('RGB') # Get current screen
 while True:
-    new_image = ImageGrab.grab().convert('RGB')
-    sleep(2)
+    new_image = ImageGrab.grab().convert('RGB') # Get new screen
+    sleep(TIMEOUT)
     diff = ImageChops.difference(new_image, image)
     if diff.getbbox():
         new_image.save(f"screen_{datetime.now()}.png")
